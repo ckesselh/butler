5f5d366c611c7adfe0bb4f6582d64513f4fe0c68
Christian Hillerkus <chris@kinsoft.eu>
2026-09-03 13:17:59 +0200

    release: 0.7.0 (#19)

--- package.nix
+++ package.nix
@@ -14,7 +14,7 @@ stdenv.mkDerivation {
   pname = "butler";
   # Keep in sync with build.zig.zon (the authoritative copy); CI asserts the
   # two agree.
-  version = "0.6.0";
+  version = "0.7.0";
 
   src = ./.;
 
